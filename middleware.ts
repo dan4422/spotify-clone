efa2c2b254d20f3c95c2408034de2215e929006c
Dan4422 <sdlee095@gmail.com>
2022-11-23 16:54:22 -0500

    data fetching and fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,16 @@ import { NextResponse } from "next/server";
 
 const signedinPages = ["/", "/playlist", "/library"];
 
-export default function middleware(req: {
-  nextUrl: {
-    clone(): unknown;
-    pathname: string;
-  };
-  cookies: { TRAX_ACCESS_TOKEN: any };
-  NextRequest: any;
-}) {
+export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
+    const token = req.cookies.get("TRAX_ACCESS_TOKEN");
 
     const url = req.nextUrl.clone();
-    url.pathname = "/signin";
     if (!token) {
+      url.pathname = "/signin";
       return NextResponse.redirect(url);
     }
+    // url.pathname = "/";
+    // return NextResponse.redirect(url);
   }
 }
