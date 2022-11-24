9d52190a469055ff87ce400a9188f165b0e2d7aa
Dan4422 <sdlee095@gmail.com>
2022-11-23 19:22:15 -0500

    finished homepage UI and added migrations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,52 @@
-import GradientLayout from '../components/gradientLayout';
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import GradientLayout from "../components/gradientLayout";
+import { useMe } from "../lib/hooks";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists }) => {
+  const { user, isLoading, error } = useMe();
+
   return (
     <GradientLayout
       roundImage
       color="blue"
       subtitle="profile"
-      title="Daniel Lee"
-      description="15 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user.playlistsCount} public playlists`}
       image="https://frontendmasters.github.io/fullstack-app-next-website/images/profile.png"
     >
-      <div>Home Page</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">Top Artist this Month</Text>
+          <Text fontSize="medium">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="http://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="xs">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
